e4045eed2f8434663f1e8a1c5a00cac0f5ec9f35
sabiden <sabiden@stuy.edu>
2015-11-24 20:28:21 -0500

    PHASE 4 COMPLETE

---
+++ Rational.java
@@ -0,0 +1,201 @@
+//Team Sing Sung
+//APCS1 pd10
+//HW37 -- Rational Equality
+//2015-11-25
+
+public class Rational {
+
+
+    private int numerator;
+    private int denominator;
+
+    public Rational() {
+	numerator = 0;
+	denominator = 1;
+    }
+
+    public Rational(int n, int d) {
+	if ( d == 0 ) {
+	    System.out.println("Nope.");
+	    numerator = 0;
+	    denominator = 1;
+	}
+	else {
+	    numerator = n;
+	    denominator = d;
+	}
+    }
+
+
+    public String toString() { 
+	return numerator + " / " + denominator;
+    }
+
+    public double floatValue() { 
+	return numerator/(double)denominator;
+    }
+
+    public void multiply(Rational s) {
+	numerator *= s.numerator;
+	denominator *= s.denominator;
+    }
+
+    public void divide(Rational s) {
+	numerator *= s.denominator;
+	denominator *= s.numerator;
+    }
+
+    public void add(Rational s) {
+	if ( denominator == s.denominator ) {
+	    numerator += s.numerator;
+	}
+	else {
+	    numerator *= s.denominator;
+	    numerator += s.numerator * denominator;
+	    denominator *= s.denominator;
+	}
+    }
+
+    public void subtract(Rational s) {
+	if ( denominator == s.denominator ) {
+	    numerator -= s.numerator;
+	}
+	else {
+	    numerator *= s.denominator;
+	    numerator -= s.numerator * denominator;
+	    denominator *= s.denominator;
+	}
+    }
+
+    public int gcd(){
+	int min;
+	int max;
+	int stor;
+	if ((numerator == 0) || (denominator == 0)){
+	    return 0;
+	}
+	else {
+	    if ( numerator >= denominator ) {
+		max = numerator;
+		min = denominator;
+	    }
+	    else {
+		max = denominator;
+		min = numerator;
+	    }
+	    while (min != 0){
+	        stor = min;
+		min = max % min;
+		max = stor;
+	    }
+	    return max;
+	}
+    }
+
+    public static int gcds(int n, int d) {
+	int min;
+	int max;
+	int stor;
+	if ((n == 0) || (d == 0)){
+	    return 0;
+	}
+	else {
+	    if ( n >= d ) {
+		max = n;
+		min = d;
+	    }
+	    else {
+		max = d;
+		min = n;
+	    }
+	    while (min != 0){
+	        stor = min;
+		min = max % min;
+		max = stor;
+	    }
+	    return max;
+	}
+    }
+
+
+    public void reduce() {
+	int g = this.gcd();
+	if ( g != 0 ) {
+	    numerator /= g;
+	    denominator /= g;
+	}
+    }
+
+    public int compareTo(Rational r) {
+        this.reduce();
+	r.reduce();
+	//Stimulates the idea of cross-multiplication so that the fractions have the same denominator, and then compares a and b which are the numerator values
+	int a = this.numerator * r.denominator;
+	int b = this.denominator * r.numerator;
+       	if ( a == b ) {
+	    return 0;
+	}
+	else if ( a < b ) {
+	    return -1;
+	}
+	else {
+	    return 1;
+	}
+    }
+
+    public boolean equals(Rational r) {
+	if(this == r){
+	    return true;
+	}
+	if(this.compareTo(r) == 0){
+	    return true;
+	}
+	return false;
+    }
+
+    public static void main( String[] args ) { // main method
+
+	// testing!
+	
+	Rational a = new Rational(10,5);
+	System.out.println(a);
+	System.out.println(a.floatValue());
+	System.out.println(a.gcd());
+	a.reduce();
+	System.out.println(a);
+
+	
+	Rational b = new Rational(2,10);
+	System.out.println(b);
+	System.out.println(b.floatValue());
+	System.out.println(b.gcd());
+	b.reduce();
+	System.out.println(b);
+
+
+	Rational c = new Rational(9,14);
+	System.out.println(c);
+	System.out.println(c.floatValue());
+	System.out.println(c.gcd());
+	c.reduce();
+	System.out.println(c);
+
+	
+	c.add(b);
+	System.out.println(c);
+	a.subtract(b);
+	System.out.println(a);
+
+	Rational val1 = new Rational(15,30);
+	Rational val2 = new Rational(3,6);
+	Rational val3 = new Rational(5,6);
+
+	System.out.println(val1.compareTo(val2));
+	System.out.println(val1.compareTo(val3));
+	System.out.println(val1.equals(val2));
+	System.out.println(val1.equals(val3));
+	
+    }
+}
+    
+    
